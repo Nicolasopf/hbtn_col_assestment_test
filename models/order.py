e063beca82281e017f131260444471854b70f564
Nicolas <n.ico15935746@gmail.com>
2021-06-30 03:25:58 -0500

    [Added] Order class to represent orders table.

---
+++ models/order.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Orders class '''
+from sqlalchemy import Column, String, DateTime, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from datetime import datetime
+
+
+class Order(BaseModel, Base):
+    ''' Table Order for the orders. '''
+    __tablename__ = "orders"
+    date = Column(DateTime, default=datetime.utcnow)
+    total = Column(String(60), nullable=False)
+    subtotal = Column(String(60), nullable=False)
+    taxes = Column(String(60), nullable=False)
+    paid = Column(String(60), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'))
+    user = relationship("User", back_populates="order")
+
+    def __init__(self, *args, **kwargs):
+        ''' Intialize the order. '''
+        super().__init__(*args, **kwargs)
