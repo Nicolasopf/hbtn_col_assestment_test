4779b53cf5247961c5b7212d19b22abd00718a00
Nicolas <n.ico15935746@gmail.com>
2021-06-30 03:19:01 -0500

    [Added] Setup the database

--- setup.sql
+++ setup.sql
@@ -1,5 +1,5 @@
 -- Create the database and user for the application.
-CREATE DATABASE IF NOT EXISTS 'hbtn_col_db';
+CREATE DATABASE IF NOT EXISTS hbtn_col_db;
 CREATE USER IF NOT EXISTS 'hbtn_col'@'localhost' IDENTIFIED BY 'hbtn_col_pwd';
-GRANT ALL PRIVILEGES ON 'hbtn_col_db'.* TO 'hbtn_col'@'localhost';
+GRANT ALL PRIVILEGES ON `hbtn_col_db`.* TO 'hbtn_col'@'localhost';
 FLUSH PRIVILEGES;
